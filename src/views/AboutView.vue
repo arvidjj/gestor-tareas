6dd1e1336f74dd8c94d349655a3c109d9c501448
arvidjj <andy.oj14@gmail.com>
2023-08-08 16:19:37 -0400

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <h1>About</h1>
+      <h2>Sistemas Distribuidos: Practico#1</h2>
+    </div>
+    <hr>
+    <div>
+      <p>Probando Vue para utilizarlo en la materia.</p>
+      <br>
+      <p>Se utiliza vue.draggable.next para los click&drags, LocalStorage al cargar la vista home</p>
+    </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
